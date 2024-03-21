cffdc00c152b0dddf4b3c64f19afe43b5b36f445
Jonathan Kong <jonathankong1996@gmail.com>
2024-03-21 23:41:29 +0800

    feat: add context and repsonsive homepage - added context for product data from `fakeapistore` - made homepage responsive across all viewports

---
+++ contexts/ProductContext.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { createContext, useState, useEffect } from 'react';
+
+// create context
+export const ProductContext = createContext({});
+
+const ProductProvider = ({ children }: any) => {
+  // products state
+  const [products, setProducts] = useState([]);
+  // fetch products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('https://fakestoreapi.com/products');
+      const data = await res.json();
+      // console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
