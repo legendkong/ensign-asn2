85e9e0929968a22e5349ec698389de70c47af456
Jonathan Kong <jonathankong1996@gmail.com>
2024-03-22 21:26:14 +0800

    feat: imlement cart items and other ui changes on main page

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+function NotFoundPage() {
+  return <h1>supposedly the product description page 😆</h1>;
+}
+
+export default NotFoundPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
 'use client';
-import Image from 'next/image';
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 import Sidebar from '@/components/Sidebar';
 import Footer from '@/components/Footer';
 import { useContext } from 'react';
 import ProductProvider, { ProductContext } from '@/contexts/ProductContext';
 import Product from '@/components/Product';
 import SidebarProvider from '@/contexts/SidebarContext';
+import CartProvider from '@/contexts/CartContext';
 
 function HomeContent() {
   // get products from product contetx
   const { products }: any = useContext(ProductContext);
 
   const filteredProducts = products.filter((item: any) => {
-    return (
-      item.category === "men's clothing" || item.category === "women's clothing"
-    );
+    return item.category === 'electronics' || item.category === 'jewelery';
   });
   // console.log(filteredProducts);
   return (
     <div>
       <Header />
+
       <section className="py-16">
+        <Hero />
         <div className="container mx-auto">
           <div
             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm
@@ -39,12 +40,14 @@ function HomeContent() {
   );
 }
 
-export default function Home() {
+export default function Home({ pageProps }: any) {
   return (
     <SidebarProvider>
-      <ProductProvider>
-        <HomeContent />
-      </ProductProvider>
+      <CartProvider>
+        <ProductProvider>
+          <HomeContent />
+        </ProductProvider>
+      </CartProvider>
     </SidebarProvider>
   );
 }
